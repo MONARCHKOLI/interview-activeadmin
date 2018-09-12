e4e4326d9fcc763ae72fc63a16970f63b6319a93
Sebastian Armano <sebarmano@gmail.com>
2018-09-12 11:48:36 -0400

    Add first failing test

--- spec/features/patient_completes_phq_spec.rb
+++ spec/features/patient_completes_phq_spec.rb
@@ -8,8 +8,8 @@ RSpec.feature "A patient checks into the app" do
 
     expect(page).to have_content "Please complete all of the steps on this page"
 
-    click_on "Complete check in"
+    click_on "Start PHQ screener"
 
-    expect(page).to have_button "Start check in"
+    expect(page).to have_content("Over the past 2 weeks, how often have you been bothered by any of the following problems?")
   end
 end
